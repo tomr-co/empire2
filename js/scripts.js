591e942db1ef3e484e074f329d5eca670a9a2690
tomr-co <tomr.design@gmail.com>
2016-06-23 18:55:15 +1200

    burger menu working on mobile

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,11 @@
-// Unslider Variable. 
-
-var slider = $('.my-slider').unslider();
-
 // Burger Menu 
+
+// Hide our Navigation 
+
+$("#nav").addClass("js");
+
+// Add click listener to Burger menu + Toggle visability of the nav.
+
+$(".menu-box").click(function(){
+	$("#nav").toggle();
+});
